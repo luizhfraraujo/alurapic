8e2b82f7f944f2a78081fc7fea558aaad773c55b
Luiz Henrique <luizhfreitas@outlook.com>
2016-09-21 13:47:49 -0300

    Aula 15

--- app/routes/fotos.js
+++ app/routes/fotos.js
@@ -1,7 +1,10 @@
 module.exports = function(app) {
 
     var api = app.api.fotos;
-    app.get('/v1/fotos', api.lista);
-    app.get('/v1/fotos/:id', api.buscaPorId);
 
+    app.get('/v1/fotos', api.lista);
+
+    app.route('/v1/fotos/:id')
+        .get(api.buscaPorId)
+        .delete(api.removePorId);
 }
