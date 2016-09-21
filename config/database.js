a98c1ed962a958fa4cc8d724f26a3c4ada8c3db5
Luiz Henrique <luizhfreitas@outlook.com>
2016-09-21 16:07:55 -0300

    MongoDB

--- config/database.js
+++ config/database.js
@@ -2,6 +2,22 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://tir4y:tir4y@ds023912.mlab.com:23912/testes');
 
-mongoose.connection.on('connected', function(){
+mongoose.connection.on('connected', function() {
     console.log('Conectado ao MongoDB');
 });
+/*
+mongoose.connection.on('error', function(error) {
+    console.log('Erro na Conexão ao MongoDB:' + error);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Desconectado do MongoDB');
+});
+
+process.on('SIGINT', function() {
+
+    mongoose.connection.close(function() {
+        console.log('Aplicação fechada. Conexão fechada pelo término da aplicação.');
+        //process.exit(0);
+    });
+});*/
