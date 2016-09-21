d99ee07f8802f0b004ed878cd2359a0799cf7d64
Luiz Henrique <luizhfreitas@outlook.com>
2016-09-21 16:27:04 -0300

    Aula 21

--- config/database.js
+++ config/database.js
@@ -1,23 +1,29 @@
-var mongoose = require('mongoose');
+module.exports = function(uri) {
+    var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://tir4y:tir4y@ds023912.mlab.com:23912/testes');
+    mongoose.connect(uri);
 
-mongoose.connection.on('connected', function() {
-    console.log('Conectado ao MongoDB');
-});
-/*
-mongoose.connection.on('error', function(error) {
-    console.log('Erro na Conexão ao MongoDB:' + error);
-});
+    mongoose.connection.on('connected', function() {
 
-mongoose.connection.on('disconnected', function() {
-    console.log('Desconectado do MongoDB');
-});
-
-process.on('SIGINT', function() {
-
-    mongoose.connection.close(function() {
-        console.log('Aplicação fechada. Conexão fechada pelo término da aplicação.');
-        //process.exit(0);
+        console.log('Conectado ao MongoDB');
     });
-});*/
+
+    mongoose.connection.on('error', function(error) {
+
+        console.log('Erro na Conexão ao MongoDB:' + error);
+    });
+
+    mongoose.connection.on('disconnected', function() {
+
+        console.log('Desconectado do MongoDB');
+    });
+
+    process.on('SIGINT', function() {
+
+        mongoose.connection.close(function() {
+            console.log('Aplicação fechada. Conexão fechada pelo término da aplicação.');
+            process.exit(0);
+        });
+    });
+    /**/
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-var db = require('./config/database');
+var db = require('./config/database')('mongodb://tir4y:tir4y@ds023912.mlab.com:23912/testes');
 
 //criar um servidor
 http.createServer(app).listen(3000, function() {
